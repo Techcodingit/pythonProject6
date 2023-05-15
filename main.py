28234fcd30ad682c9d3a3f44f935471ee6fe8790
USER <ruppa.santosh143darling@gmail.com>
2023-05-15 23:25:01 +0530

    my first fibonacci sequence method

--- main.py
+++ main.py
@@ -1,61 +1,19 @@
-# append method to add value
-r = ['santosh', 'devops', True, 3, 5, 9]
-s = [3, 6, 7, 8, 1, 4]
-s.append(77)
-print(s)
-######################
-# remove method to remove a value from list
-r = ['santosh', 'devops', True, 3, 5, 9]
-s = [3, 6, 7, 8, 1, 4]
-r.remove('santosh')
-print(r)
-################################
-# count function used to know ... howmany times are there a repeated value in index posision
-r = ['santosh', 'devops', True, 3, 5, 9]
-s = [3, 6, 3, 8, 3, 3]
-print(s.count(3))
-# here i want to know howmany times are there 3 in s thats why i put 3 after count
-#########################
-# here i used extend method to add multiple values to the list or add another list to exsting list
-a = ['santosh', 'devops', True, 3, 5, 9]
-b = [3, 6, 7, 8, 1, 4]
-a.extend([99, 'python', 88,])
-print(a)
-################################
-# here i used pop() method to remove index posision.
-a = ['santosh', 'devops', True, 3, 5, 9]
-b = [3, 6, 7, 8, 1, 4]
-b.pop(2)
-print(b)
-##############################
-# i used reverse method to reverse all values in index posision
-a = ['santosh', 'devops', True, 3, 5, 9]
-b = [3, 6, 7, 8, 1, 4]
-b.reverse()
-print(b)
-###################################
-# i used assending order(small to big) to arrange numbers small to big
-a = ['santosh', 'devops', True, 3, 5, 9]
-b = [3, 6, 7, 8, 1, 4]
-b.sort()
-print(b)
-#############################
-# Python3 program to multiply all values in the
-# list using math.prod
+n = int(input("enter length of fibonacci sequence"))
 
-import math
-list1 = [1, 2, 3]
-list2 = [3, 2, 4]
-#math.prod() method in Python is used to calculate the product of all the elements present in the given iterable.
-result1 = math.prod(list1)
-result2 = math.prod(list2)
-print(result1)
-print(result2)
-############################
+n1 = 0
+n2 = 1
+count = 2
 
-
-
-
-
-
-
+if n<0:
+    print("enter only +ve values")
+elif n==1:
+    print(n1)
+else:
+    print(n1,end=" ")
+    print(n2,end=" ")
+    while count<n:
+        n3=n1+n2
+        print(n3, end=" ")
+        count+=1
+        n1=n2
+        n2=n3
